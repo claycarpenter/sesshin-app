70db158fd3c52badc063e0f0f2053fb090f85a49
Clay Carpenter <claycarpenter@gmail.com>
2016-06-05 10:44:52 -0400

    Adds skeleton Meditate page.

---
+++ app/pages/meditate/meditate.page.ts
@@ -0,0 +1,11 @@
+import {Page} from 'ionic-angular';
+
+
+@Page({
+  templateUrl: 'build/pages/meditate/meditate.page.html',
+})
+export class MeditatePage {
+  constructor() {
+
+  }
+}
--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,5 +1,5 @@
 import {Page} from 'ionic-angular';
-import {Page1} from '../page1/page1';
+import {MeditatePage} from '../meditate/meditate.page';
 import {Page2} from '../page2/page2';
 import {Page3} from '../page3/page3';
 
@@ -8,9 +8,7 @@ import {Page3} from '../page3/page3';
   templateUrl: 'build/pages/tabs/tabs.html'
 })
 export class TabsPage {
-  // this tells the tabs component which Pages
-  // should be each tab's root Page
-  tab1Root: any = Page1;
+  meditateTabRoot: any = MeditatePage;
   tab2Root: any = Page2;
   tab3Root: any = Page3;
 }
